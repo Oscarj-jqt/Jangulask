bda8b50f38c41ec701fcfb0e6b9e47d5e6eba500
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-27 15:24:00 +0100

    Initialize Spring Boot app

---
+++ Springulask_server/Springulask_server/src/main/java/com/codewithprojects/Springulask_server/SpringulaskServerApplication.java
@@ -0,0 +1,13 @@
+package com.codewithprojects.Springulask_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringulaskServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringulaskServerApplication.class, args);
+	}
+
+}
---
+++ Springulask_server/Springulask_server/src/test/java/com/codewithprojects/Springulask_server/SpringulaskServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithprojects.Springulask_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringulaskServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
