0acf311278497572e58314fed02a926624714482
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-01 15:01:23 +0100

    Create a Storage Service & Role Base Navigation in Angular

--- springulask_web/src/app/auth/components/login/login.component.ts
+++ springulask_web/src/app/auth/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Routes } from '@angular/router';
+import { StorageService } from '../../services/storage/storage.service';
 
 @Component({
   selector: 'app-login',
@@ -13,11 +15,11 @@ export class LoginComponent {
 
   loginForm! : FormGroup;
   hidePassword = true;
-  routes: any;
 
   constructor(private fb: FormBuilder,
     private authService: AuthService,
-    private snackbar: MatSnackBar
+    private snackbar: MatSnackBar,
+    private routes: Routes
   ) {
     this.loginForm = this.fb.group({
       email:[null,[Validators.required,Validators.email]],
@@ -34,6 +36,16 @@ export class LoginComponent {
     this.authService.login(this.loginForm.value).subscribe((res) => {
       console.log(res);
       if (res.userId != null) {
+        const user = {
+          id: res.userId,
+          role: res.userRole
+        }
+        StorageService.saveUser(user);
+        StorageService.saveToken(res.jwt);
+        if (StorageService.isAdminLoggedIn())
+          this.routes.navigateByUrl("/admin/dashboard");
+        else if (StorageService.isEmployeeLoggedIn())
+          this.routes.navigateByUrl("/employee/dashboard");
         this.snackbar.open("Login successful", "Close", { duration: 5000 });
       } else {
         this.snackbar.open("Invalid credentials", "Close", { duration:5000, panelClass: "error-snackbar" })
