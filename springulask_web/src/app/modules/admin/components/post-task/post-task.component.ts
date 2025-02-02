1c005adb16b92bdb8fd32bbc87aa0a4dde4a4035
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-02 11:35:28 +0100

    Create a Post Task Reactive Form in Angular

--- springulask_web/src/app/modules/admin/components/post-task/post-task.component.ts
+++ springulask_web/src/app/modules/admin/components/post-task/post-task.component.ts
@@ -1,20 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-post-task',
-  imports: [],
+  imports: [
+  ],
   templateUrl: './post-task.component.html',
   styleUrl: './post-task.component.css'
 })
 export class PostTaskComponent {
 
-  constructor(private adminService: AdminService) {
+taskForm!: FormGroup;
+listOfEmployees: any[];
+listOfPriorities: any = ["LOW", "MEDIUM", "HIGH"];
+
+  constructor(private adminService: AdminService,
+    private fb: FormBuilder
+  ) {
     this.getUsers();
+    this.taskForm = this.fb.group({
+      employeeId: [null, [Validators.required]],
+      title: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      dueDate: [null, [Validators.required]],
+      priority: [null, [Validators.required]],
+    })
   }
 
   getUsers() {
     this.adminService.getUsers().subscribe((res) => {
+      this.listOfEmployees = res;
       console.log(res);
     })
+  }
+
+  postTask() {
+    console.log(this.taskForm.value);
   }
 }
