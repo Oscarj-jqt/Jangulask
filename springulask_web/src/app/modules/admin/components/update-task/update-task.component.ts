f126772d8b2055d9ec20a7b358998bbd776a2f40
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-03 12:54:02 +0100

    Creating Update Task Reactive Form & Calling Update Task API in Angular

--- springulask_web/src/app/modules/admin/components/update-task/update-task.component.ts
+++ springulask_web/src/app/modules/admin/components/update-task/update-task.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-update-task',
@@ -10,7 +11,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UpdateTaskComponent {
 
-  id:number = this.route.snapshot.params["id"];
+  id: number = this.route.snapshot.params["id"];
+  taskForm!: FormGroup;
+  listOfEmployees: any = [];
+  listOfPriorities: any = ["LOW", "MEDIUM", "HIGH"];
 
   constructor(private service: AdminService,
     private route: ActivatedRoute
