c970121b28d5b6c4cabd3656194e763065f6e299
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-03 15:15:10 +0100

    Create Search Task By Title API in Spring Boot

--- Springulask_server/Springulask_server/src/main/java/com/codewithprojects/Springulask_server/controller/admin/AdminController.java
+++ Springulask_server/Springulask_server/src/main/java/com/codewithprojects/Springulask_server/controller/admin/AdminController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
@@ -55,6 +57,11 @@ public class AdminController {
         TaskDTO updatedTask = adminService.updateTask(id, taskDTO);
         if (updatedTask == null) return ResponseEntity.notFound().build();
         return ResponseEntity.ok(updatedTask);
+    }
+
+    @GetMapping("/tasks/search/{title}")
+    public ResponseEntity<List<TaskDTO>> searchTask(@PathVariable String title) {
+        return ResponseEntity.ok(adminService.searchTaskByTitle(title));
     }
 
 }
--- Springulask_server/Springulask_server/src/main/java/com/codewithprojects/Springulask_server/repositories/TaskRepository.java
+++ Springulask_server/Springulask_server/src/main/java/com/codewithprojects/Springulask_server/repositories/TaskRepository.java
@@ -1,10 +1,14 @@
 package com.codewithprojects.Springulask_server.repositories;
 
 
+import com.codewithprojects.Springulask_server.dto.TaskDTO;
 import com.codewithprojects.Springulask_server.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task,Long> {
+    List<Task> findAllByTitleContaining(String title);
 }
